0f500477b5c1b2eddd7383c09a07dee7f4555038
000VIJAY <vijay893689@gmail.com>
2022-08-06 15:45:03 +0530

    Vijay Added program for create contact

--- addressbook.js
+++ addressbook.js
@@ -1,1 +1,21 @@
-console.log("Welcome to addressbook problem")
+
+class contactFunc {
+    firstName;lastName;address;city;state;zip;phoneNumber;email;
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    toString()
+    {
+       return "First Name: "+this.firstName+", Last Name: "+this.lastName+", Address: "+this.address+", city: "+this.city+", State: "+this.state+
+        ", Zip Code: "+this.zip+", Phone Number: "+this.phoneNumber+", Email: "+this.email;
+    }
+}
+let contact = new contactFunc("Vijay","Kumar","1-2-3Jankipuram","Lucknow","Bihar",768990,8847285433,"Vijay@gmail.com")
+console.log(contact.toString());
